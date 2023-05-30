e95b520e20a025deb3b98992607de064d8f31cd5
chelishvili15 <chelo@redberry.ge>
2023-05-30 12:48:59 +0400

    tic-tac-toe setup

---
+++ tic-tac-toe/src/App.jsx
@@ -0,0 +1,10 @@
+function App() {
+  return (
+    <div className='bg-gray-100 flex flex-col justify-center items-center h-screen'>
+      <span className="text-5xl">Hello React + tailwind</span>
+      <span className="text-2xl">Tic Tac Toe</span>
+    </div>
+  )
+}
+
+export default App
