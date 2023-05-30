6f6c337a0df1154e41e2ade2e2b1264a3c124941
chelishvili15 <chelo@redberry.ge>
2023-05-30 18:22:43 +0400

    feat(tictactoe): add scores funtionality

--- tic-tac-toe/src/App.jsx
+++ tic-tac-toe/src/App.jsx
@@ -4,7 +4,7 @@ import { Board } from "./components/Board"
 import { Scores } from "./components/Scores"
 
 function App() {
-  const winner = [
+  const winPositions = [
     [0,1,2],
     [3,4,5],
     [6,7,8],
@@ -16,6 +16,7 @@ function App() {
   ]
   const [board, setBoard] = useState(Array(9).fill(null))
   const [xPlayed, setXPlayed] = useState(true)
+  const [scores, setScores] = useState({xScore: 0, oScore: 0})
 
   const handleBoardClick = (boxIndex) => {
     if (!board[boxIndex]) {
@@ -26,15 +27,29 @@ function App() {
           return val
         }
       })
-  
-      checkWinner(updateBoard)
+
       setBoard(updateBoard)
+  
+      if (checkWinner(updateBoard)) {
+        let {xScore, oScore} = scores
+
+        if (checkWinner(updateBoard) === 'X') {
+          xScore += 1
+          setScores({ xScore, oScore })
+          setTimeout(() => resetBoard() ,100)
+        } else if (checkWinner(updateBoard) === 'O') {
+          oScore += 1
+          setScores({ xScore, oScore })
+          setTimeout(() => resetBoard() ,100)
+        }
+      }
+
       setXPlayed(!xPlayed)
     }
   }
 
   const checkWinner = (board) => {
-    for (let i of winner) {
+    for (let i of winPositions) {
       const [a, b, c] = i
       if (board[a] && board[a] === board[b] && board[b] === board[c]) {
         return board[a]
@@ -44,12 +59,17 @@ function App() {
     return null
   }
 
+  const resetBoard = () => {
+    const resetBoard = board.map(() => null)
+    setBoard(resetBoard)
+  }
+
   return (
     <div className='bg-gray-100 flex flex-col justify-center items-center h-screen'>
-      <span className="text-5xl">Hello React + tailwind</span>
+      <span className="text-5xl">React + tailwind</span>
       <span className="text-2xl">Tic Tac Toe</span>
       <div>
-        <Scores xScore={10} oScore={7} isActiveX={xPlayed} />
+        <Scores xScore={scores.xScore} oScore={scores.oScore} isActiveX={xPlayed} />
         <Board board={board} onClick={handleBoardClick} />
       </div>
     </div>
